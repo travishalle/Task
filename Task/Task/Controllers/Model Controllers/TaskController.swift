66402edc1bc3a1a3dc3f3dfea5627b7ae35a3610
Comet <travishalle@mac.com>
2021-04-21 21:24:37 -0400

    Task commit 4/21/2021

---
+++ Task/Task/Controllers/Model Controllers/TaskController.swift
@@ -0,0 +1,8 @@
+//
+//  TaskController.swift
+//  Task
+//
+//  Created by Travis Halle on 4/21/21.
+//
+
+import Foundation
---
+++ Task/Task/Views/Custom Views/TaskTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  TaskTableViewCell.swift
+//  Task
+//
+//  Created by Travis Halle on 4/21/21.
+//
+
+import UIKit
+
+class TaskTableViewCell: UITableViewCell {
+
+    
+
+}//End of class
